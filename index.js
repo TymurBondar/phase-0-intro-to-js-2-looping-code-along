645970e5dac217f354cd2ab913223868efa9d8ce
Tymur Bondar <tymurbondar@outlook.com>
2023-08-30 04:55:43 -0400

    loops

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, gift){
+    const messages = []
+    names.forEach(element => {
+        messages.push(`Thank you, ${element}, for the wonderful ${gift} gift!`);
+    });
+    return messages;
+}
+
+//writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(n){
+    while (0 <= n){
+        console.log(n);
+        n--;
+    }
+}
